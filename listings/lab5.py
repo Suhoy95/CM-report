cbd59ef8b278db0981151d74997fce7fc49c5ab6
Ilya Sukhoplyuev <suhoy95@gmail.com>
2017-11-13 12:58:00 +0500

    fixes

--- listings/lab5.py
+++ listings/lab5.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 ny = 1     # Параметр системы
-h = 0.0001 # шаг метода Эйлера 
+h = 0.0001 # шаг метода Эйлера
            # и основание прямоугольников при интегрировании
 
 
@@ -15,13 +15,13 @@ def line(y1_0, y2_0):
     y2 = [y2_0]
     eps = 0.5 * 10 ** -4
     for i in range(100000):
-        y1.append(y1[-1] + h * (y2[-1]))
-        y2.append(y2[-1] +
-                  h*(-3*y2[-1] ** 3 + ny * y2[-1] - y1[-1]))
-        if (np.abs(y1_0 - y1[-1]) < eps and 
-            np.abs(y2_0 - y2[-1]) < eps):
+        y1.append(y1[i] + h * (y2[i]))
+        y2.append(y2[i] +
+                  h*(-3*y2[i] ** 3 + ny * y2[i] - y1[i]))
+        if (np.abs(y1_0 - y1[i + 1]) < eps and
+            np.abs(y2_0 - y2[i + 1]) < eps):
             return (y1, y2)
-    
+
     raise Exception("Cycle not found")
 
 
@@ -37,6 +37,6 @@ def integral_from_div(cycle_y1, cycle_y2):
     return sum
 
 # Основная программа
-cycle_y1, cycle_y2 = line(0.724197, 0)
+cycle_y1, cycle_y2 = line(0.72424, 0)
 s = integral_from_div(cycle_y1, cycle_y2)
 print("Integral of the divergence: {}".format(s))
